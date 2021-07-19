c42b2d4e23cf01373421923bb130759f8455681f
cizordj <32869222+cizordj@users.noreply.github.com>
2021-07-19 20:22:38 -0300

    Fix the Emoji crash
    - Fix crash that happens in the terminal when a colored emoji is
      displayed

--- x.c
+++ x.c
@@ -1318,7 +1318,7 @@ xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len, int x
 			FcCharSetAddChar(fccharset, rune);
 			FcPatternAddCharSet(fcpattern, FC_CHARSET,
 					fccharset);
-			FcPatternAddBool(fcpattern, FC_SCALABLE, 1);
+			FcPatternAddBool(fcpattern, FC_SCALABLE, FcFalse);
 
 			FcConfigSubstitute(0, fcpattern,
 					FcMatchPattern);
