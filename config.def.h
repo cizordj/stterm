6b661ff28f93d36b9d7d328271362192b37ba36e
cizordj <32869222+cizordj@users.noreply.github.com>
2021-07-30 22:34:51 -0300

    Update my preferences
    - Change the default cursor
    - Change the default background color to black

--- config.def.h
+++ config.def.h
@@ -135,7 +135,7 @@ unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-unsigned int bg = 17, bgUnfocused = 16;
+unsigned int bg = 16, bgUnfocused = 16;
 
 /*
  * Default shape of cursor
@@ -144,7 +144,7 @@ unsigned int bg = 17, bgUnfocused = 16;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
